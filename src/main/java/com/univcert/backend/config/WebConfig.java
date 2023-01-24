1fdb1b90584ae9dbd8e71aa7ab1e4fe8af87aee7
in-seo <sis000512@gmail.com>
2023-01-24 13:51:17 +0900

    err: CORS mapping(3)

--- src/main/java/com/univcert/backend/config/WebConfig.java
+++ src/main/java/com/univcert/backend/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOriginPatterns("*")
                 .allowedMethods("*")
                 .allowCredentials(true)
                 .maxAge(3000);
